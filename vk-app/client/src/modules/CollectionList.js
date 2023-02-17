78300102e238edf8e712a82f3fa5be99ce7983de
Anatoliy Sergeev <64964053+sergeevtoll@users.noreply.github.com>
2023-02-18 00:10:34 +0800

    fix: theme button

--- vk-app/client/src/modules/CollectionList.js
+++ vk-app/client/src/modules/CollectionList.js
@@ -2,9 +2,9 @@ import { CardGrid, Div, Spinner, Card, Button, Title, Spacing } from "@vkontakte
 import { useCollectionList } from "../hooks/useCollectionList"
 import { shortAddress } from "../utils/shortAddress"
 
+const buttonGridStyle = { width: '100%', height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '20px', padding: '20px' }
 
 export const CollectionList = ({ selectCollection, goToCreateCollection }) => {
-  const buttonGridStyle = { width: '100%', height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '20px', padding: '20px' }
   const { list, load } = useCollectionList()
   if (load) return (
     <Div>
